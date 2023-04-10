aea9756a0f1f544bbc72fb24cf6974a743ce2bce
Bu Weiyu <vivianbu16@gmail.com>
2023-04-10 21:25:28 +0800

    pic done

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,8 @@ user = User.create!(
 )
 
 # TODO: Creating 5 sectors
+puts "Creating sectors"
+
 sectors = [
   Sector.create!(name: "Education"),
   Sector.create!(name: "Enterprise"),
@@ -40,6 +42,9 @@ sectors = [
   Sector.create!(name: "Aerospace & Space"),
   Sector.create!(name: "Climate & Sustainability")
 ]
+
+puts "Creating founders"
+
 founder = Founder.new(
   user: user,
   company_name: "CP Ventures",
@@ -55,6 +60,8 @@ founder.sectors = sectors.sample(3)
 founder.save!
 # TODO: Creating 5 investors (investor_profile)
 # load the investors.json file into memory
+puts "Creating investors"
+
 investors_json = File.read(File.join(__dir__, 'investors.json'))
 # parse the json file as ruby
 investors = JSON.parse(investors_json)
@@ -85,19 +92,19 @@ investor_photos = [
  "https://images.unsplash.com/photo-1613473350016-1fe047d6d360?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzl8fGJ1c2luZXNzJTIwcG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://plus.unsplash.com/premium_photo-1661284837474-58ca893cc879?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjkyfHxlbnRyZXByZW5ldXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1648474484044-bb82df2f5a1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjQ2fHxidXNpbmVzcyUyMHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
- "https://images.unsplash.com/photo-1615538785945-6625ccdb4b25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjUxfHxidXNpbmVzcyUyMHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+ "https://images.unsplash.com/photo-1625249927451-ba4973d5b222?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODR8fHdvcmslMjBwbGFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1572274407649-6cb883740677?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzExfHxlbnRyZXByZW5ldXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1655249493799-9cee4fe983bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjcyfHxidXNpbmVzcyUyMHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1573497019418-b400bb3ab074?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzEyfHxlbnRyZXByZW5ldXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://plus.unsplash.com/premium_photo-1661284828986-b565e6b13fd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDI0fHxlbnRyZXByZW5ldXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1648484099915-b72a39454f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjI3fHxidXNpbmVzcyUyMHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
- "https://images.unsplash.com/photo-1664318454883-8100a89f6745?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjYzfHxidXNpbmVzcyUyMHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+ "https://plus.unsplash.com/premium_photo-1663011410518-3421bc33f8f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODV8fHdvcmslMjBwbGFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1573166475912-1ed8b4f093d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDM0fHxlbnRyZXByZW5ldXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1655249481446-25d575f1c054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAwN3x8YnVzaW5lc3MlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://plus.unsplash.com/premium_photo-1661277712617-1e46eac647e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDQyfHxlbnRyZXByZW5ldXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1665224752136-4dbe2dfc8195?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE0NHx8YnVzaW5lc3MlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1644442146822-97c05b44e45d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE0MXx8YnVzaW5lc3MlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
- "https://images.unsplash.com/photo-1601058660898-82c168404c88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDY0fHxlbnRyZXByZW5ldXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+ "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdvcmslMjBwbGFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1610896011476-300d6239d995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTI2Nnx8YnVzaW5lc3MlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1620246499808-d9fe1b7858dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTI3OHx8YnVzaW5lc3MlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1627161684458-a62da52b51c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTMxNXx8YnVzaW5lc3MlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
@@ -136,16 +143,16 @@ investor_photos = [
  "https://images.unsplash.com/photo-1573497701175-00c200fd57f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzExfHxwcm9mZXNzaW9uYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1631306006348-f9ed42b1e19a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzQ0fHxwcm9mZXNzaW9uYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDY2fHxwcm9mZXNzaW9uYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
- "https://images.unsplash.com/photo-1600878459081-02f46f27b406?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDg1fHxwcm9mZXNzaW9uYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+ "https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1570126618953-d437176e8c79?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcGFueXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1462206092226-f46025ffe607?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y29tcGFueXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
- "https://images.unsplash.com/photo-1617526738882-1ea945ce3e56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNvbXBhbnl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+ "https://images.unsplash.com/photo-1560264280-88b68371db39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
  "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNvbXBhbnl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1606836591695-4d58a73eba1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGNvbXBhbnl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1470075801209-17f9ec0cada6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGNvbXBhbnl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1573165231977-3f0e27806045?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzJ8fGNvbXBhbnl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1551836022-aadb801c60ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTc4fHxjb21wYW55fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
- "https://images.unsplash.com/photo-1561489413-985b06da5bee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjEyfHxjb21wYW55fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+ "https://images.unsplash.com/photo-1603117791031-bed60a5acbe1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODZ8fHdvcmslMjBwbGFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1577415124269-fc1140a69e91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTl8fGJ1c2luZXNzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1541746972996-4e0b0f43e02a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE2fHxidXNpbmVzc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
  "https://images.unsplash.com/photo-1567450475250-21f5b6d98021?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTM3fHxidXNpbmVzc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
@@ -155,7 +162,7 @@ investor_photos = [
  "https://images.unsplash.com/photo-1573496130141-209d200cebd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODF8fGVudHJlcHJlbmV1cnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
 ]
 counter = 0
-investors.take(77).each do |investor|
+investors.each do |investor|
   # if investor["Founders"].empty?
   #   first_name, last_name = investor["Founders"].split(",")[0].split
   # else
@@ -163,7 +170,7 @@ investors.take(77).each do |investor|
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
 
-    Investor.create!(
+    inv = Investor.create!(
       first_name: first_name,
       last_name: last_name,
       company_name: investor["Name"],
@@ -172,7 +179,7 @@ investors.take(77).each do |investor|
       company_UEN: "202#{rand(2...3)}#{rand(9)}#{rand(9)}#{rand(9)}#{rand(9)}#{rand(9)}X",
       funding_stage: investor["Stages"],
       ticket_size: investor["Minimum Check Size"].empty? ? "$100,000" : investor["Minimum Check Size"],
-      sectors: ["Education", "Enterprise", "SaaS", "Big Data & Analytics", "Productivity", "Cybersecurity", "Supply Chain & Logistics", "Consumer", "Food & Beverage", "Future of Work", "Transportation", "Travel & Hospitality", "Healthcare", "AR & VR", "Health & Wellness", "Insuretech", "Aerospace & Space", "Climate & Sustainability"].sample(3)
+      # sectors: ["Education", "Enterprise", "SaaS", "Big Data & Analytics", "Productivity", "Cybersecurity", "Supply Chain & Logistics", "Consumer", "Food & Beverage", "Future of Work", "Transportation", "Travel & Hospitality", "Healthcare", "AR & VR", "Health & Wellness", "Insuretech", "Aerospace & Space", "Climate & Sustainability"].sample(3)
     )
 
   file = URI.open(investor_photos[counter])
@@ -191,17 +198,17 @@ investors.take(77).each do |investor|
   puts "#{first_name} #{last_name} created!"
 end
 
-file = URI.open("https://media.istockphoto.com/id/1407354641/zh/%E7%85%A7%E7%89%87/group-of-diverse-executive-managers-doing-paperwork-at-meeting.jpg?s=170667a&w=0&k=20&c=nInzvTirAqoToIRHw8ChbZroTHZz--UKkXOhY7SZwtA=")
+# file = URI.open("https://media.istockphoto.com/id/1407354641/zh/%E7%85%A7%E7%89%87/group-of-diverse-executive-managers-doing-paperwork-at-meeting.jpg?s=170667a&w=0&k=20&c=nInzvTirAqoToIRHw8ChbZroTHZz--UKkXOhY7SZwtA=")
 
-investor = Investor.new(
-  first_name: "Peter",
-  last_name: "Pan",
-  company_name: "Amazon Peter",
-  company_description: "Amazon Peter",
-  company_email: "peterpan@gmail.com",
-  company_UEN: "202002039T",
-  funding_stage: "Seed",
-  ticket_size: "$1000000",
-)
-investor.photo.attach(io: file, filename: "investor1.jpg", content_type: "image/jpg")
-investor.save!
+# investor = Investor.new(
+#   first_name: "Peter",
+#   last_name: "Pan",
+#   company_name: "Amazon Peter",
+#   company_description: "Amazon Peter",
+#   company_email: "peterpan@gmail.com",
+#   company_UEN: "202002039T",
+#   funding_stage: "Seed",
+#   ticket_size: "$1000000",
+# )
+# investor.photo.attach(io: file, filename: "investor1.jpg", content_type: "image/jpg")
+# investor.save!
